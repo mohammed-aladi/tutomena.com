a3ee5e0449de66d9bc46dd449a59710481571d48
Aissa BOUGUERN <aissa.bouguern@gmail.com>
2020-08-26 14:09:37 -0400

    Add Support Shortcode

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -2,9 +2,9 @@ import React from 'react';
 import { ThemeProvider } from './src/context/ThemeContext';
 
 import { MDXProvider } from '@mdx-js/react';
-import { NewsletterForm, Author, Quiz } from './src/shortcodes';
+import { NewsletterForm, Author, Quiz, Support } from './src/shortcodes';
 
-const shortcodes = { NewsletterForm, Author, Quiz };
+const shortcodes = { NewsletterForm, Author, Quiz, Support };
 
 export const wrapRootElement = ({ element }) => (
   <MDXProvider components={shortcodes}>
